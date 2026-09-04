e64f20edefa585ed4a2f6e0d33514be28470f4aa
MiyamotoMusasshi <wowad506hp@gmail.com>
2026-09-04 18:18:49 +0200

    Added visualization for the ingredient selection menu.

--- src/components/Customize/IngredientsMenu.vue
+++ src/components/Customize/IngredientsMenu.vue
@@ -4,6 +4,22 @@ const props = defineProps(['isClicked'])
 
 <template>
   <Transition name="menu">
-    <div class="menu" v-show="props.isClicked"></div>
+    <div class="menu" v-show="props.isClicked">
+      <div class="menu_choice">
+        <div class="ingredient">
+          <img src="../../assets/img/Sausage.svg" class="ingredient_img" />
+          <p class="ingredient_name">Пеперони</p>
+          <div class="counter">
+            <button class="counter_btn">-</button>
+            <p class="ingredient_name">0</p>
+            <button class="counter_btn">+</button>
+          </div>
+        </div>
+      </div>
+      <div class="menu_other">
+        <p class="ingredient_name price">Итоговая цена: 0р</p>
+        <button class="pay ingredient_name">Перейти к оплате -></button>
+      </div>
+    </div>
   </Transition>
 </template>
